cbe1d2e45420c1afc2e6a56b30cac45ad67b7de5
Dawid Dominik <dpolanski1999@gmail.com>
2020-10-22 14:05:13 +0200

    ItaskRepository

---
+++ TaskManager/Repositories/TaskRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManager.Models;
+
+namespace TaskManager.Repositories
+{
+    public class TaskRepository : ITaskRepository
+    {
+        public TaskModel Get(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TaskModel> GetAllActivie()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(TaskModel task)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int taskId, TaskModel task)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Delete(int taskId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
